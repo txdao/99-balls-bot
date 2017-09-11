f7e9a8a9c4e3d316d9d4ce3061be781e4b203901
Tung X. Dao <tungxdao.gmail.com>
2017-09-11 16:40:24 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# imports
+import neat
+#import screen grab
+#import mouse control
+
+def run():
+    # initialize game area
+
+    # train neural net
+
+    # save winning data etc (on interupt?).
+    pass
+
+if __name__ == '__main__':
+    run()
