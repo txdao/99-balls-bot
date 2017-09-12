a3501fb2b07bd169f23abffea333ea4f1f5e99b7
Tung X. Dao <tungxdao.gmail.com>
2017-09-11 20:36:12 -0700

    Initial check-in of branch

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 # imports
 import neat
+import webbrowser
 #import screen grab
 #import mouse control
+
+def init_game_area():
+    '''
+    create new instance of game
+    determine where the game area is
+    '''
+    pass
 
 def run():
     # initialize game area
