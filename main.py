c298fc48a649719bd20163216c4cbcbe100f4248
Tung X. Dao <tungxdao.gmail.com>
2017-09-11 22:54:09 -0700

    Create skeleton

--- main.py
+++ main.py
@@ -5,15 +5,33 @@ import webbrowser
 #import screen grab
 #import mouse control
 
+
+def launch_game_browser():
+    pass
+
+def get_screen_img():
+    return None
+
+def get_game_coords(img):
+    return None
+
 def init_game_area():
     '''
     create new instance of game
     determine where the game area is
     '''
+    # launch browser to https://www.crazygames.com/assets/99-balls/index.html
+    launch_game_browser()
+    screen_img = get_screen_img()
+    game_coords = get_game_coords(screen_img)
+
+
+
     pass
 
 def run():
     # initialize game area
+    init_game_area()
 
     # train neural net
 
