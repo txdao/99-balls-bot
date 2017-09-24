7407c5675ad9bc4c97ffde2c01021f5f295cb3ad
Tung X. Dao <tungxdao.gmail.com>
2017-09-23 19:42:10 -0700

    Create class skeleton

--- game.py
+++ game.py
@@ -5,3 +5,28 @@ Created on Thu Sep 21 17:32:35 2017
 @author: Tung X. Dao
 """
 
+class Game():
+    def __init__(self):
+        self.game_coords = []
+        self.state = []
+        pass
+
+    def start_game():
+        pass
+
+    def reset_game():
+        pass
+
+    def get_game_state():
+        return None
+
+    def circle(angle):
+        pass
+
+    def update_game_state():
+        pass
+
+    def move_game_area(direction):
+        pass
+
+
